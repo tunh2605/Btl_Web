cb6ce1cc35ee12acad56538509da224dd0673403
Nguyen Hoang Tung <nguyenhoangtung@sadboi.local>
2024-10-22 17:45:09 +0700

    updated purchase button

--- N_H_Tung/js/trailer.js
+++ N_H_Tung/js/trailer.js
@@ -1,21 +1,17 @@
-// Lấy các phần tử từ DOM
 const trailerBtn = document.getElementById('trailerBtn');
 const closeBtn = document.getElementById('closeBtn');
-const trailerContainer = document.getElementById('trailer-vid-film');
+const trailerContainer = document.getElementById('trailer-vid');
 const modalOverlay = document.getElementById('modalOverlay');
 
-// Hiển thị modal
 trailerBtn.addEventListener('click', () => {
     trailerContainer.style.display = 'block';
     modalOverlay.style.display = 'block';
 });
 
-// Ẩn modal khi nhấn nút đóng
 closeBtn.addEventListener('click', () => {
     closeModal();
 });
 
-// Hàm ẩn modal
 function closeModal() {
     trailerContainer.style.display = 'none';
     modalOverlay.style.display = 'none';
